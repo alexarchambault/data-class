086824c846056e53824d2aba13fb45afa972a5eb
Øyvind Raddum Berg <elacin@gmail.com>
2020-11-06 17:24:10 +0100

    Add cached hashcode: replace test

--- src/test/scala/dataclass/CachedHashCodeTests.scala
+++ src/test/scala/dataclass/CachedHashCodeTests.scala
@@ -3,29 +3,34 @@ package dataclass
 import utest._
 
 object CachedHashCodeTests extends TestSuite {
-  // This is extremely crude naturally, but we typically get orders of magnitude difference:
-  //  for instance 26519 < 4382593
-  private def timed[T](warmup: => T)(t: => T): Long = {
-    warmup
-    val t0 = System.nanoTime()
-    t
-    System.nanoTime() - t0
-  }
-
   val tests = Tests {
-    "cached hashCode should be much faster" - {
-      val numbers = (0 to 1000).toVector
+    "cached hashCode should work" - {
+      class HashCodeCount(value: Int) {
+        var hashCodeCount = 0
+        override def hashCode(): Int = {
+          hashCodeCount += 1
+          value
+        }
+      }
 
-      @data(cachedHashCode = true) class Cached(numbers: Seq[Int])
-      val cached = numbers.map(_ => Cached(numbers))
-      val cachedMs = timed(cached.hashCode)(cached.hashCode)
+      @data(cachedHashCode = true) class Cached(value: HashCodeCount)
+      @data class NotCached(value: HashCodeCount)
 
-      @data class NotCached(numbers: Seq[Int])
-      val notCached = numbers.map(_ => NotCached(numbers))
-      val notCachedMs = timed(notCached.hashCode)(notCached.hashCode)
+      val cached = Cached(new HashCodeCount(2))
+      val notCached = NotCached(new HashCodeCount(2))
 
-      println(s"$cachedMs < $notCachedMs")
-      assert(cachedMs < notCachedMs)
+      assert(cached.value.hashCodeCount == 0)
+      assert(notCached.value.hashCodeCount == 0)
+
+      cached.hashCode
+      notCached.hashCode
+      assert(cached.value.hashCodeCount == 1)
+      assert(notCached.value.hashCodeCount == 1)
+
+      cached.hashCode
+      notCached.hashCode
+      assert(cached.value.hashCodeCount == 1)
+      assert(notCached.value.hashCodeCount == 2)
     }
   }
 }
