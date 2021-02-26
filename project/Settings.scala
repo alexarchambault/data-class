85fb012db687b7883ebe7dbe5b7e38a33658b1e2
Scala Steward <me@scala-steward.org>
2021-02-26 22:10:26 +0100

    Update scala-library, scala-reflect to 2.13.5

--- project/Settings.scala
+++ project/Settings.scala
@@ -5,7 +5,7 @@ object Settings {
 
   def scala213 = "2.13.3"
   def scala212 = "2.12.12"
-  def scala211 = "2.11.12"
+  def scala211 = "2.13.5"
 
   private lazy val isScala211Or212 = Def.setting {
     val sv = scalaVersion.value
