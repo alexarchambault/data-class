07c0d2df027b2df5377bdeed5d24549727dddee5
Scala Steward <me@scala-steward.org>
2021-10-05 04:03:47 +0200

    Update utest to 0.7.10

--- project/Deps.scala
+++ project/Deps.scala
@@ -12,7 +12,7 @@ object Deps {
     Def.setting {
       val sv = scalaVersion.value
       val ver =
-        if (sv.startsWith("2.11.")) "0.6.8"
+        if (sv.startsWith("2.11.")) "0.7.10"
         else "0.7.1"
       "com.lihaoyi" %% "utest" % ver
     }
